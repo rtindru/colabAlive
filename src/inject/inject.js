3bdea231ece93e1e043012c97d38f738933ce357
Rajtilak Indrajit <indrajit@springboard.com>
2020-06-09 08:52:10 -0700

    Updated javascript to reconnect via the lighbox disconnecmessage

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,13 +7,27 @@ chrome.extension.sendMessage({}, function (response) {
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
-			setInterval(ClickConnect, 60000);
+			setInterval(clickConnect, 60000);
+			setInterval(ligboxConnect, 60000);
 		}
 	}, 10);
 });
 
 // Credit to https://medium.com/@shivamrawat_756/how-to-prevent-google-colab-from-disconnecting-717b88a128c0
-function ClickConnect() {
-	console.log("Keeping Colab Alive!");
-	document.querySelector("colab-connect-button").click()
+function clickConnect() {
+	try {
+		document.querySelector("colab-connect-button").click()
+		console.log("Keeping Colab Alive!");	
+	} catch (error) {
+		console.log(err);
+	}
+}
+
+function ligboxConnect() {
+	try {
+		document.querySelector("paper-dialog paper-button#ok").click()	
+		console.log('Prevented disconnection')
+	} catch (error) {
+		console.log(err);
+	}
 }
