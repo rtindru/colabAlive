2660a018c02009aea90aa428bcce37347cc2c46a
Oshayr <>
2021-01-11 00:26:36 +0200

    n

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,7 @@ chrome.extension.sendMessage({}, function (response) {
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
-			setInterval(clickConnect, 780000);
+			setInterval(clickConnect, 60000);
 		}
 	}, 10);
 });
