c03e82374687ec71d50d2cbc4d34d7ee2bae4cef
Oshayr <7786820+Oshayr@users.noreply.github.com>
2021-01-10 23:36:55 +0200

    Update inject.js
    replaced inject function

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,26 +8,12 @@ chrome.extension.sendMessage({}, function (response) {
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
 			setInterval(clickConnect, 780000);
-			setInterval(ligboxConnect, 780010);
 		}
 	}, 10);
 });
 
-// Credit to https://medium.com/@shivamrawat_756/how-to-prevent-google-colab-from-disconnecting-717b88a128c0
-function clickConnect() {
-	try {
-		document.querySelector("colab-connect-button").click()
-		console.log("Keeping Colab Alive!");	
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-function ligboxConnect() {
-	try {
-		document.querySelector("paper-button dialog-dismiss").click()	
-		console.log('Prevented disconnection')
-	} catch (error) {
-		console.log(error);
-	}
+function ClickConnect(){
+{
+console.log(“Working”);
+document.querySelector(“colab-connect-button”).shadowRoot.getElementById(‘connect’).click();
 }
