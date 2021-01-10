6b08b8c14e01958edfb03cab2fab1df46271a1bf
Oshayr <7786820+Oshayr@users.noreply.github.com>
2021-01-11 00:26:20 +0200

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,13 +7,17 @@ chrome.extension.sendMessage({}, function (response) {
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
-			setInterval(clickConnect, 780000);
+			setInterval(clickConnect, 120000);
 		}
 	}, 10);
 });
 
-function ClickConnect(){
-{
-console.log(“Working”);
-document.querySelector(“colab-connect-button”).shadowRoot.getElementById(‘connect’).click();
+function clickConnect() {
+	try {
+		document.querySelector("colab-connect-button").click()
+		console.log("Keeping Colab Alive!");	
+	} catch (error) {
+		console.log(error);
+	}
 }
+
