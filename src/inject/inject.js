47c86b5cba3b3e2b371593cfed42006e2c9dbf46
Oshayr <7786820+Oshayr@users.noreply.github.com>
2021-01-23 21:38:58 +0200

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,8 +7,8 @@ chrome.extension.sendMessage({}, function (response) {
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
 			// ----------------------------------------------------------
-			//setInterval(clickConnect, 720000);	//12 minutes
-			setInterval(clickConnect, 20000);	//12 minutes
+			setInterval(clickConnect, 720000);	//12 minutes
+			//setInterval(clickConnect, 20000);	//20 sec
 		}
 	}, 10);
 });
